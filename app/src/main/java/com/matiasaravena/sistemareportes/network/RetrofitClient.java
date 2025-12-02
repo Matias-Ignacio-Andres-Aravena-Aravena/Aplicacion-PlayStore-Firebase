4bfb3f758a17e57a6ea08d6f8ddd7755d759c0db
Matias Aravena <aravenamatias4@gmail.com>
2025-12-02 17:31:52 -0300

    Update RetrofitClient.java

--- app/src/main/java/com/matiasaravena/sistemareportes/network/RetrofitClient.java
+++ app/src/main/java/com/matiasaravena/sistemareportes/network/RetrofitClient.java
@@ -6,8 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 	
 	private static Retrofit retrofit = null;
-	// NOTA: 10.0.2.2 es la dirección "localhost" del emulador de Android.
-	// Si usas tu celular real por USB, aquí iría la IP de tu PC (ej: "http://192.168.1.15:8000/api/")
 	private static final String BASE_URL = "http://10.0.2.2:8000/api/";
 	
 	public static ApiService getApiService() {
